6c67d451b7ec74b10e7a67d96d85973236f509ad
alkaz07 <>
2023-08-02 12:27:34 +0300

    сделали Cat и Dog сделали методы геттеры и сеттеры в них

--- src/ClassesAndMethods.java
+++ src/ClassesAndMethods.java
@@ -1,5 +1,54 @@
 public class ClassesAndMethods {
     public static void main(String[] args) {
-        
+        exemple1();
+    }
+
+    static void exemple1() {
+        Cat kitty1=new Cat(), kitty2 = new Cat();
+        kitty1.setName("Барсик");
+        kitty2.setName("Клеопатра");
+        System.out.println(kitty1.getName());
+        kitty1.setName("Наполеон");
+        System.out.println(kitty1.getName());
+        //kitty2.name = "hjkljl";
+    }
+}
+
+class Cat {
+    private String name;
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String s)
+    {
+        System.out.println("было имя "+name+" станет "+s);
+        name = s;
+    }
+}
+
+class Dog{
+    private String name;
+    private String breed;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        if(this.breed == null)
+            this.breed = breed;
+        else
+            System.out.println("поздно, порода уже задана");
     }
 }
